123de42937e806872b215b7ab0a0a2ef58004355
ekane3 <ekaneekane3@gmail.com>
2020-09-25 09:53:41 +0200

    initialisation projet

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MBOA PROJECT</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			
+		</div>
+		<div class="row">
+			
+		</div>
+	</div>
+</body>
+</html>
